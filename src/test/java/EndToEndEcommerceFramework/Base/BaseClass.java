1e440cb9846d9fb36ca92ab6df8756eb45fddcfc
RelinaTiwari <relinatiwari@gmail.com>
2025-04-30 13:50:25 +0530

    changes made for jenkins

--- src/test/java/EndToEndEcommerceFramework/Base/BaseClass.java
+++ src/test/java/EndToEndEcommerceFramework/Base/BaseClass.java
@@ -7,10 +7,13 @@ import java.time.Duration;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -29,10 +32,30 @@ public class BaseClass {
 		prop.load(fis);
 		System.out.println("Hello");
 
-		String browser = prop.getProperty("browser");
+		// For reading from properties file
+		// String browser = prop.getProperty("browser");
 
-		if (browser.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
+		// For reading from compiler
+
+		// mvn test -PRegression -Dbrowser=Edge
+		String browser = System.getProperty("browser") != null 
+			    ? System.getProperty("browser") 
+			    : prop.getProperty("browser");
+
+			browser = browser.toLowerCase(); // normalize the string
+
+		if (browser.contains("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			if (browser.equalsIgnoreCase("ChromeHeadless")) {
+				options.addArguments("--headless");
+			}
+			driver = new ChromeDriver(options);
+			driver.manage().window().setSize(new Dimension(1440, 900)); // For maximized browser view when opened in
+																		// headless
+		}
+
+		else if (browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
 		}
 
 		driver.manage().window().maximize();
@@ -40,27 +63,24 @@ public class BaseClass {
 
 		return driver;
 	}
-	
-	@BeforeMethod (alwaysRun = true)
+
+	@BeforeMethod(alwaysRun = true)
 	public LandingPage launchApplication() throws IOException {
 		initializeBrowser();
 		landingpage = new LandingPage(driver);
 		landingpage.goToWebsite();
 		return landingpage;
 	}
-	
-	@AfterMethod (alwaysRun = true)
-	public void tearDown()
-	{
+
+	@AfterMethod(alwaysRun = true)
+	public void tearDown() {
 		driver.quit();
 	}
-	
-	
-	public String getScreenShot(String testcaseName , WebDriver driver) throws IOException
-	{
-		TakesScreenshot ts = (TakesScreenshot)driver;
+
+	public String getScreenShot(String testcaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
 		File source = ts.getScreenshotAs(OutputType.FILE);
-		String path = System.getProperty("user.dir")+"//reports//"+testcaseName+".png";
+		String path = System.getProperty("user.dir") + "//reports//" + testcaseName + ".png";
 		File dest = new File(path);
 		FileUtils.copyFile(source, dest);
 		return path;
